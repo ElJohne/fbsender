f51176ab30ac00f417bebe1fe2116667ba640ce5
ElJohne <ivansishchuk02@gmail.com>
2021-11-07 20:04:47 +0200

    test modifying UserController

--- src/main/java/games/ani/fbsender/controller/UserController.java
+++ src/main/java/games/ani/fbsender/controller/UserController.java
@@ -1,8 +1,18 @@
 package games.ani.fbsender.controller;
 
 
+import games.ani.fbsender.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
 }
